f1aacff83e8c2c5a4f9998d89e4ea5ef19d6de69
Amie Everett <amie.everett@gmail.com>
2020-12-06 18:36:34 -0500

    ♻️  Refactor for part 2 of day one

--- challenge1/solution1.py
+++ challenge1/solution1.py
@@ -2,11 +2,16 @@ with open('/Users/amieeverett/Sites/advent-of-code-2020/challenge1/input.txt') a
     expenses = [int(i.strip()) for i in f.readlines()]
 print(expenses)
 
-while len(expenses) > 0:
-    potentialExpense = expenses.pop()
-    for expense in expenses:
-        if potentialExpense + expense == 2020:
-            print('solution')
-            print(potentialExpense, expense)
-            print(potentialExpense*expense)
-            break
+def findAddendsForSum(listOfAddends, summationAmount):
+    while len(listOfAddends) > 0:
+        potentialAddend1 = listOfAddends.pop()
+        for potentialAddend2 in listOfAddends:
+            if potentialAddend1 + potentialAddend2 == summationAmount:
+                print('Addend 1, Addend 2:')
+                print(potentialAddend1, potentialAddend2)
+                print('Product of Addend 1 * Addend 2:')
+                print(potentialAddend1*potentialAddend2)
+                break
+
+
+findAddendsForSum(expenses, 2020)
