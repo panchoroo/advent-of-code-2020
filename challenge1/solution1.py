db2b4228706ecc1beecba17e1a09d46bf1082657
Amie Everett <amie.everett@gmail.com>
2020-12-07 10:42:38 -0500

    ✨ Add solution for day one part two

--- challenge1/solution1.py
+++ challenge1/solution1.py
@@ -7,11 +7,18 @@ def findAddendsForSum(listOfAddends, summationAmount):
         potentialAddend1 = listOfAddends.pop()
         for potentialAddend2 in listOfAddends:
             if potentialAddend1 + potentialAddend2 == summationAmount:
-                print('Addend 1, Addend 2:')
-                print(potentialAddend1, potentialAddend2)
-                print('Product of Addend 1 * Addend 2:')
-                print(potentialAddend1*potentialAddend2)
-                break
+                print('Addend 1, Addend 2:', potentialAddend1, potentialAddend2)
+                return(potentialAddend1*potentialAddend2)
 
+# Part One solution:
+# findAddendsForSum(expenses, 2020)
 
-findAddendsForSum(expenses, 2020)
+# Part Two:
+#  For each # in the list, pop it off, subtract from 2020, then see if two other numbers add up to that
+while len(expenses) > 0:
+    potentialAddend3 = expenses.pop()
+    addendsForSum = findAddendsForSum(expenses.copy(), 2020 - potentialAddend3)
+    if (addendsForSum):
+        print('potentialAddend3 is:', potentialAddend3)
+        print('addendsForSum returned:', addendsForSum)
+        print('Product of all 3 is:', potentialAddend3*addendsForSum)
