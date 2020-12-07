c489f6cd73bd94a8fdbdca3e8aa42a5c24421475
Amie Everett <amie.everett@gmail.com>
2020-12-07 18:07:08 -0500

    ✨ Add solution for day two part two

--- day2/solution.py
+++ day2/solution.py
@@ -7,12 +7,26 @@ with open('/Users/amieeverett/Sites/advent-of-code-2020/day2/input.txt') as f:
 
 # How many passwords are valid according to their policies?
 # min/max and letter to test for
+# validPasswords = 0
+# for passwordInfo in passwords:
+#     info, password = str(passwordInfo).split(':')
+#     numberRange, letter = info.split(' ')
+#     minRange, maxRange = str(numberRange).split('-')
+#     if int(minRange) <= password.count(letter) <= int(maxRange):
+#         validPasswords += 1
+# print('number of valid passwords is:', validPasswords)
+
+# part two --------------------------------------------------------------
+# Exactly one of these two positions must contain the given letter.
+# no index 0
 validPasswords = 0
 for passwordInfo in passwords:
-    info, password = str(passwordInfo).split(':')
+    info, password = str(passwordInfo).split(': ')
     numberRange, letter = info.split(' ')
     minRange, maxRange = str(numberRange).split('-')
-    if int(minRange) <= password.count(letter) <= int(maxRange):
+    firstPosition = int(minRange)-1
+    secondPosition = int(maxRange)-1
+    if ((password[firstPosition] == letter) != (password[secondPosition] == letter)):
         validPasswords += 1
 
 print('number of valid passwords is:', validPasswords)
