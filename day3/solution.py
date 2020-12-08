22bb4fd72507af8befd19ecad391d5596aa8b417
Amie Everett <amie.everett@gmail.com>
2020-12-07 20:06:53 -0500

    ✨ Add solution for day 3 part two

--- day3/solution.py
+++ day3/solution.py
@@ -1,19 +1,32 @@
-# select lines and hit Shift + Enter to run them
-# use exit() if python terminal breaks in vscode:
-# https: // stackoverflow.com/questions/51540391/invalid-syntax-error-when-running-python-from-inside-visual-studio-code
-
 with open('/Users/amieeverett/Sites/advent-of-code-2020/day3/input.txt') as f:
     rows = [i.strip() for i in f.readlines()]
 
-numberOfTrees=0
-index=0
-for row in rows:
-    if (row[index] == '#'):
-        numberOfTrees += 1
+def numberOfTreesFinder(mapOfRows, slopeX, slopeY):
+    numberOfTrees=0
+    index=0
+    rowCount=0
+    for row in mapOfRows:
+        if(rowCount % slopeY == 0):
+            if (row[index] == '#'):
+                numberOfTrees += 1
 
-    if index + 3 > len(row) - 1:
-        index -= len(row) - 3
-    else:
-        index += 3
+            if index + slopeX > len(row) - 1:
+                index -= len(row) - slopeX
+            else:
+                index += slopeX
+        rowCount += 1
+    return numberOfTrees
 
-print('numberOfTrees', numberOfTrees)
+# Right 1, down 1.
+# Right 3, down 1. (This is the slope you already checked.)
+# Right 5, down 1.
+# Right 7, down 1.
+# Right 1, down 2.
+
+# print(numberOfTreesFinder(rows, 1, 1))
+# print(numberOfTreesFinder(rows, 3, 1))
+# print(numberOfTreesFinder(rows, 5, 1))
+# print(numberOfTreesFinder(rows, 7, 1))
+# print(numberOfTreesFinder(rows, 1, 2))
+
+print(numberOfTreesFinder(rows, 1, 1)*numberOfTreesFinder(rows, 3, 1)*numberOfTreesFinder(rows, 5, 1)*numberOfTreesFinder(rows, 7, 1)*numberOfTreesFinder(rows, 1, 2))
