182b9b7410b743c91d9a5d5315ffcc58bdd6c095
Amie Everett <amie.everett@gmail.com>
2020-12-07 19:35:06 -0500

    ✨ Add solution for day 3 part one

--- day3/solution.py
+++ day3/solution.py
@@ -3,4 +3,17 @@
 # https: // stackoverflow.com/questions/51540391/invalid-syntax-error-when-running-python-from-inside-visual-studio-code
 
 with open('/Users/amieeverett/Sites/advent-of-code-2020/day3/input.txt') as f:
-    passwords = [i.strip() for i in f.readlines()]
+    rows = [i.strip() for i in f.readlines()]
+
+numberOfTrees=0
+index=0
+for row in rows:
+    if (row[index] == '#'):
+        numberOfTrees += 1
+
+    if index + 3 > len(row) - 1:
+        index -= len(row) - 3
+    else:
+        index += 3
+
+print('numberOfTrees', numberOfTrees)
