7481053b81b814e001ba146353da94bfa4e2d814
Amie Everett <amie.everett@gmail.com>
2020-12-13 09:52:57 -0500

    ✨ Add solution for day 8 part 2

--- day8/solution.py
+++ day8/solution.py
@@ -1,17 +1,12 @@
 with open("/Users/amieeverett/Sites/advent-of-code-2020/day8/input.txt") as f:
     rows = [i.strip() for i in f.readlines()]
-with open("/Users/amieeverett/Sites/advent-of-code-2020/day8/test-input.txt") as f:
-    testRows = [i.strip() for i in f.readlines()]
-with open("/Users/amieeverett/Sites/advent-of-code-2020/day8/test-input2.txt") as f:
-    testRows2 = [i.strip() for i in f.readlines()]
+# with open("/Users/amieeverett/Sites/advent-of-code-2020/day8/test-input.txt") as f:
+#     testRows = [i.strip() for i in f.readlines()]
 
-# instructions = testRows
-instructions = rows
-
-def getInstruction(index, indices, accumulator):
+def getInstruction(index, indices, accumulator, instructions):
     # print('index, indices, accumulator', index, indices, accumulator)
     if index in indices:
-        return accumulator
+        return (accumulator)
     else: 
         indices.append(index)
         command, value = instructions[index].split(" ")
@@ -22,10 +17,35 @@ def getInstruction(index, indices, accumulator):
             index += int(value)
         else:
             index += 1
-        return getInstruction(index, indices, accumulator)
-    return accumulator
+        
+        if index in range(0, len(instructions)):
+            return getInstruction(index, indices, accumulator, instructions)
+        elif index == len(instructions):
+            print("🎯 Solution: ", accumulator)
+            return (accumulator)
+        else:
+            return("Index Error")
 
-print('🎯',  getInstruction(0, [], 0))
+# print('🎯',  getInstruction(0, [], 0, rows))
 
 # ------------------------- Part 2 -------------------------
 
+# Fix the program so that it terminates normally by changing exactly one jmp(to nop) or nop(to jmp). What is the value of the accumulator after the program terminates?
+# when len(indices) = len(instructions) ?
+
+# accum, length = getInstruction(0, [], 0, rows)
+# print(accum, length)
+
+def alterInstructions(instructions, oldRowIndex, newRow, oldRow):
+    instructions[oldRowIndex] = newRow
+    print(getInstruction(0, [], 0, instructions))
+    instructions[oldRowIndex] = oldRow
+
+for rowIndex, row in enumerate(rows):
+    command, value = row.split(" ")
+    if command == "nop":
+        alterInstructions(rows, rowIndex, ' '.join(["jmp", value]), row)
+    elif command == "jmp":
+        alterInstructions(rows, rowIndex, ' '.join(["nop", value]), row)
+    else:
+        pass
