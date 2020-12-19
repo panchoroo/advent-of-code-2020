15bcec36889c0cf155ffe31814525a6ecb516d19
Amie Everett <amie.everett@gmail.com>
2020-12-18 19:06:52 -0500

    ✨ Add solution for day 10 part 2

--- day10/solution.py
+++ day10/solution.py
@@ -1,3 +1,4 @@
+import sys
 file = "test-input"
 file = "test-input2"
 file = "input"
@@ -5,27 +6,52 @@ file = "input"
 with open("/Users/amieeverett/Sites/advent-of-code-2020/day10/"+file+".txt") as f:
     rows = [int(i.strip()) for i in f.readlines()]
 
-# Find a chain that uses all of your adapters to connect the charging outlet to your device's built-in adapter and count the joltage differences between the charging outlet, the adapters, and your device. What is the number of 1-jolt differences multiplied by the number of 3-jolt differences?
+rows.append(0)
+rows.sort()
+orderedJoltages = rows
+countRuns=[1]
+def differenceCounter(orderedJoltages):
+    joltDiff1=1
+    joltDiff3=1
+    countRuns=[]
+    onesCounter=1
+    for index in range(0,len(orderedJoltages)-1):
+        difference = orderedJoltages[index+1]-orderedJoltages[index]
+        if difference == 1:
+            joltDiff1 += 1
+            onesCounter += 1
+            if onesCounter < 3:
+                countRuns.append(orderedJoltages[index])
+        elif difference == 3:
+            joltDiff3 += 1
+            onesCounter = 1
+        else:
+            print('Error')
+    return (joltDiff1, joltDiff3, countRuns)
 
-orderedJoltages=[0]
-while len(rows) > 0:
-    orderedJoltages.append(min(rows))
-    rows.remove(min(rows))
-
-# print(orderedJoltages)
-joltDiff1=0
-joltDiff3=1
-for index in range(0,len(orderedJoltages)-1):
-    difference = orderedJoltages[index+1]-orderedJoltages[index]
-    # print('orderedJoltages[index+1]-orderedJoltages[index]', orderedJoltages[index+1], orderedJoltages[index])
-    if difference == 1:
-        joltDiff1 += 1
-    elif difference == 3:
-        joltDiff3 += 1
-    else:
-        print('ruh roh')
-
-print('joltDiff1, joltDiff3', joltDiff1, joltDiff3)
-print(joltDiff1*joltDiff3)
+jolt1, jolt3, startsOfRuns = differenceCounter(rows)
+# print('startsOfRuns', startsOfRuns)
 
 # --- Part Two ---
+
+# print('orderedJoltages', orderedJoltages)
+graph = {}
+for i in orderedJoltages:
+    graph[i] = []
+
+for i in orderedJoltages:
+    for j in range(1,4):
+        if i+j in orderedJoltages:
+            graph[i].append(i+j)
+
+def PathCounter(node):
+    if len(graph[node]) <= 1:
+        return 1
+    return sum([PathCounter(child) for child in graph[node]])
+
+newSum = 1
+for g in startsOfRuns:
+    if len(graph[g]) > 1:
+        newSum *= PathCounter(g)
+
+print('newSum', newSum)
